17fc536564163f1572b1cae52965bea4eeb7bf10
rajeev <rajeevkumar25112002@gmail.com>
2024-12-24 18:59:06 +0530

    learning about variable

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "rajeevkumar3443@gmail.com"
+var accountPassword = "123430"
+accountCity = "jaipur"
+
+// accountId = 21  const value are not allowed to change.
+// var does not have control on the scope. so we have to use let 
+accountEmail = "dfadsf@gmail.com"
+accountPassword = "1234"
+console.log(accountId)
